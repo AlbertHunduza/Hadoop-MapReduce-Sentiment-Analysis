2c413c6965312f85d808948ad28c9fa73215428a
hunduza <hunduza@uni-hildesheim.de>
2023-07-02 11:44:30 +0200

    Fixed folder and file names

--- hadoop_code/run.sh
+++ hadoop_code/run.sh
@@ -1,12 +1,12 @@
-rm -r out
+rm -r output
 rm -r hdfs
 mkdir hdfs
-hdfs dfs -put inpt.txt hdfs/inpt.txt
+hdfs dfs -put input.txt hdfs/input.txt
 hadoop jar /opt/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar \
     -files mapper.py \
     -mapper "python3 mapper.py" \
     -file reducer.py \
     -reducer "python3 reducer.py" \
-    -input hdfs/inpt.txt \
-    -output out
-cat out/part-00000
+    -input hdfs/input.txt \
+    -output output
+cat output/part-00000
