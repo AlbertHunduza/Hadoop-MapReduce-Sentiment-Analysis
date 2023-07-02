bab92e715ddda02bb416db91116eeb36dbce8b4f
hunduza <hunduza@uni-hildesheim.de>
2023-07-02 11:49:33 +0200

    added twitter file

--- hadoop_code/run.sh
+++ hadoop_code/run.sh
@@ -1,12 +1,12 @@
 rm -r output
 rm -r hdfs
 mkdir hdfs
-hdfs dfs -put input.txt hdfs/input.txt
+hdfs dfs -put twitter.txt hdfs/twitter.txt
 hadoop jar /opt/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar \
     -files mapper.py \
     -mapper "python3 mapper.py" \
     -file reducer.py \
     -reducer "python3 reducer.py" \
-    -input hdfs/input.txt \
+    -input hdfs/twitter.txt \
     -output output
 cat output/part-00000
